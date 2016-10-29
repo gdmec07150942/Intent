9fa653d3000760c545eb7a1995f0f3738c2cb7ab
gdmec07150942 <554061700@qq.com>
2016-10-29 12:01:15 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/intent/IntentDemo3.java
@@ -0,0 +1,19 @@
+package com.example.administrator.intent;
+
+/**
+ * Created by Administrator on 2016/10/29.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yejs on 2016/10/29.
+ */
+public class IntentDemo3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
